adcbf2494c9f076ecbdc3c6359b3e39d51ed2dc3
mksathish64-sk <mksathish64@gmail.com>
2026-07-11 15:04:53 +0530

    V2:Added more operations

--- Linkedlist/LinkedList.java
+++ Linkedlist/LinkedList.java
@@ -55,12 +55,86 @@ public class LinkedList {
             temp = temp.next;
             if(temp == null)
             {
-                throw new IndexOutOfBoundsException("Error While inserting at the pos"+pos);
+                throw new IndexOutOfBoundsException("Error While inserting at the pos:"+pos);
             }
         }
         newNode.next = temp.next;
         temp.next = newNode;
     }
 
+    public void deleteAtPos(int pos)
+    {
+        if(head == null)
+        {
+            throw new IndexOutOfBoundsException("Deletion attemoted at empty list");
+        }
+        if(pos == 0)
+        {
+            head = head.next;
+            return;
+        }
+        Node temp = head;
+        Node prev = head;
+        for(int i=1;i<=pos;i++)
+        {
+            prev = temp;
+            temp = temp.next;
+        }
+        prev.next = temp.next;
+
+    }
+    public int get(int pos)
+    {
+        if(head == null)
+        {
+            throw new IndexOutOfBoundsException();
+        }
+        Node temp = head;
+        for(int i=1;i<=pos;i++)
+        {
+            temp = temp.next;
+            if(temp == null)
+            {
+                throw new IndexOutOfBoundsException();
+            }
+        }
+        return temp.data;
+    }
+    public void update(int pos,int val)
+    {
+        if(head == null)
+        {
+            throw new IndexOutOfBoundsException();
+        }
+        Node temp = head;
+        for(int i=1;i<=pos;i++)
+        {
+            temp = temp.next;
+            if(temp == null)
+            {
+                throw new IndexOutOfBoundsException();
+            }
+        }
+        temp.data = val;
+    }
+    public int search(int val)
+    {
+        if(head == null)
+        {
+            throw new IndexOutOfBoundsException("List is Empty");
+        }
+        Node temp = head;
+        int index = 0;
+        while(temp!=null)
+        {
+            if(temp.data == val)
+            {
+                return index;
+            }
+            index++;
+            temp = temp.next;
+        }
+        return -1;
+    }
     
 }
--- Linkedlist/Test.java
+++ Linkedlist/Test.java
@@ -1,12 +1,78 @@
 package Linkedlist;
 
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args)
     {
+        Scanner sc = new Scanner(System.in);
         LinkedList list = new LinkedList();
-        list.insertAtBeginning(5);
-        list.insertAtBeginning(9);
-        list.insertAtPos(1,4);
-        list.display();
+        int choice,val,pos;
+        while(true)
+        {
+        System.out.println("\n--------------- Menu ---------------");
+        System.out.println("01. Insert at Beginning");
+        System.out.println("02. Insert at Particular Position");
+        System.out.println("03. Delete at particular position");
+        System.out.println("04. get");
+        System.out.println("05. Update");
+        System.out.println("06. search");
+        System.out.println("07. Display");
+        System.out.println("08. Exit");
+        System.out.println("Enter your choice: ");
+        choice = sc.nextInt();
+        switch(choice)
+        {
+            case 1:
+                System.out.println("Enter the Element to insert:");
+                val = sc.nextInt();
+                list.insertAtBeginning(val);
+                break;
+            case 2:
+                System.out.println("Enter the Position(strats at 0):");
+                pos = sc.nextInt();
+                System.out.println("Enter the element to insert:");
+                val = sc.nextInt();
+                list.insertAtPos(pos,val);
+                break;
+            case 3:
+                System.out.println("Enter the Position(strats at 0):");
+                pos = sc.nextInt();
+                list.deleteAtPos(pos);
+                break;
+            case 4:
+                System.out.println("Enter the Position(strats at 0):");
+                pos = sc.nextInt();
+                int data = list.get(pos);
+                System.out.println("The data in the Position "+pos+" is "+data);
+                break;  
+            case 5:
+                System.out.println("Enter the Position(strats at 0):");
+                pos = sc.nextInt();
+                System.out.println("Enter the element to update:");
+                val = sc.nextInt();
+                list.update(pos,val);
+                break;    
+            case 6:
+                System.out.println("Enter the Element to search:");
+                val = sc.nextInt();
+                int index = list.search(val);
+                if(index!=-1)
+                {
+                    System.out.println("Element found at "+index);
+                }
+                else
+                {
+                    System.out.println("Not found");
+                }
+                break;      
+            case 7:
+                list.display();
+                break;
+            case 8: System.exit(0);
+            default:
+                System.out.println("!!!Invalid Choice!!!");
+        }
+        }
     }
 }
